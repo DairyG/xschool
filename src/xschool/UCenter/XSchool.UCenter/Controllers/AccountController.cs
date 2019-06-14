92c333927ae9ab8dd441c0723363501470cace64
popo <popo_520@outlook.com>
2019-06-14 18:09:04 +0800

    Auto stash before merge of "master" and "origin/master"

---
+++ src/xschool/UCenter/XSchool.UCenter/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XSchool.UCenter.Controllers
+{
+    public class AccountController: ControllerBase
+    {
+
+        public IActionResult Login()
+        {
+            return null;
+        }
+
+
+        public IActionResult LoginOff()
+        {
+            return null;
+        }
+    }
+}

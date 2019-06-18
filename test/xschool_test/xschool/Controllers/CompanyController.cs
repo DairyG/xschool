8ef6894088304833247cfb2c2bd0e0f1f664b5a5
popo <popo_520@outlook.com>
2019-06-18 18:13:22 +0800

    Auto stash before merge of "master" and "origin/master"

---
+++ test/xschool_test/xschool/Controllers/CompanyController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XShop.GCenter.DoMain;
+
+namespace xschool.Controllers
+{
+    public class CompanyTestDo
+    {
+        private XShop.GCenter.Businesses.CompanyBusiness _companyBusiness;
+        private XShop.GCenter.Businesses.BankInfoBusiness _bankInfoBusiness;
+        public CompanyTestDo(XShop.GCenter.Businesses.CompanyBusiness companyBusiness, XShop.GCenter.Businesses.BankInfoBusiness bankInfoBusiness)
+        {
+            this._companyBusiness = companyBusiness;
+            this._bankInfoBusiness = bankInfoBusiness;
+        }
+
+        public void Excute(Action action)
+        {
+
+        }
+    }
+
+
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]/[action]")]
+    public class CompanyController:ControllerBase
+    {
+        private readonly CompanyDo _companyDo;
+        public CompanyController(CompanyDo companyDo)
+        {
+            this._companyDo = companyDo;
+        }
+
+        public IActionResult Test()
+        {
+            return null;
+        }
+    }
+}

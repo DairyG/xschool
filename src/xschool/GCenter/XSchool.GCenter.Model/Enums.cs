32cb308ed58a9ea25de9a83b84bb1d5c63ea4567
悟鱼 <592164556@qq.com>
2019-06-26 20:57:30 +0800

    删除公司、银行账户、人员、部门

--- src/xschool/GCenter/XSchool.GCenter.Model/Enums.cs
+++ src/xschool/GCenter/XSchool.GCenter.Model/Enums.cs
@@ -3,24 +3,6 @@
 namespace XSchool.GCenter.Model
 {
     /// <summary>
-    /// 银行账户类型
-    /// </summary>
-    public enum AccountType
-    {
-        /// <summary>
-        /// 基本账户
-        /// </summary>
-        [Description("基本账户")]
-        Basic = 1,
-
-        /// <summary>
-        /// 一般账户
-        /// </summary>
-        [Description("一般账户")]
-        Normal = 2,
-    }
-
-    /// <summary>
     /// 常用状态，[有效，无效]
     /// </summary>
     public enum NomalStatus
@@ -54,54 +36,6 @@ namespace XSchool.GCenter.Model
         /// </summary>
         [Description("启用")]
         Enable = 2,
-    }
-
-    /// <summary>
-    /// 员工在职状态
-    /// </summary>
-    public enum PersonStatus
-    {
-        /// <summary>
-        /// 未入职
-        /// </summary>
-        [Description("未入职")]
-        Unknown = 0,
-
-        /// <summary>
-        /// 试用
-        /// </summary>
-        [Description("试用")]
-        Trial = 1,
-
-        /// <summary>
-        /// 转正
-        /// </summary>
-        [Description("转正")]
-        Positive = 2,
-
-        /// <summary>
-        /// 离职
-        /// </summary>
-        [Description("离职")]
-        Departure = -1,
-    }
-
-    /// <summary>
-    /// 性别
-    /// </summary>
-    public enum Gender
-    {
-        /// <summary>
-        /// 男
-        /// </summary>
-        [Description("男")]
-        Man = 1,
-
-        /// <summary>
-        /// 女
-        /// </summary>
-        [Description("女")]
-        Woman = 2,
     }
 
     /// <summary>
@@ -166,6 +100,24 @@ namespace XSchool.GCenter.Model
     }
 
     /// <summary>
+    /// 性别
+    /// </summary>
+    public enum Gender
+    {
+        /// <summary>
+        /// 男
+        /// </summary>
+        [Description("男")]
+        Man = 1,
+
+        /// <summary>
+        /// 女
+        /// </summary>
+        [Description("女")]
+        Woman = 2,
+    }
+
+    /// <summary>
     /// 是否为系统数据
     /// </summary>
     public enum IsSystem
@@ -199,24 +151,6 @@ namespace XSchool.GCenter.Model
         /// </summary>
         [Description("扣除")]
         Subtraction = 2,
-    }
-
-    /// <summary>
-    /// 员工 添加/编辑 操作的选项卡
-    /// </summary>
-    public enum PersonOperation
-    {
-        /// <summary>
-        /// 人员信息
-        /// </summary>
-        [Description("人员信息")]
-        PersonInfo = 1,
-
-        /// <summary>
-        /// 职位信息
-        /// </summary>
-        [Description("职位信息")]
-        PositionInfo = 2,
     }
 
     /// <summary>
--- src/xschool/GCenter/XSchool.GCenter.Repositories/Extensions/GCenterDbContext.cs
+++ src/xschool/GCenter/XSchool.GCenter.Repositories/Extensions/GCenterDbContext.cs
@@ -7,13 +7,9 @@ namespace XSchool.GCenter.Repositories.Extensions
     {
         public GCenterDbContext(DbContextOptions<GCenterDbContext> options) : base(options) { }
 
-        public DbSet<Company> Company { get; set; }
-        public DbSet<BankInfo> bankInfo { get; set; }
-        public DbSet<Department> Department { get; set; }
         public DbSet<WorkerInFieldSetting> WorkerInFieldSetting { get; set; }
         public DbSet<BonusPenaltySetting> BonusPenaltySetting { get; set; }
         public DbSet<PositionSetting> PositionSetting { get; set; }
-        public DbSet<Person> Person { get; set; }
         public DbSet<Budget> Budget { get; set; }
         public DbSet<Resume> Resume { get; set; }
     }

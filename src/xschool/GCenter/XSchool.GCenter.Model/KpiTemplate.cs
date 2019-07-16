4542c08e095bccde22065ce0242af43678560869
悟鱼 <592164556@qq.com>
2019-07-16 09:47:16 +0800

    冲突解决

---
+++ src/xschool/GCenter/XSchool.GCenter.Model/KpiTemplate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using XSchool.Core;
+
+namespace XSchool.GCenter.Model
+{
+    public class KpiTemplate : IModel<int>
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 考核类型，1-部门，2-人员
+        /// </summary>		
+        public int KpiType { get; set; }
+
+        /// <summary>
+        /// 公司Id
+        /// </summary>		
+        public int CompanyId { get; set; }
+
+        /// <summary>
+        /// 公司名称
+        /// </summary>		
+        public string CompanyName { get; set; }
+
+        /// <summary>
+        /// 部门Id
+        /// </summary>		
+        public int DptId { get; set; }
+
+        /// <summary>
+        /// 部门名称
+        /// </summary>		
+        public string DptName { get; set; }
+
+        /// <summary>
+        /// 人员Id
+        /// </summary>		
+        public int EmployeeId { get; set; }
+
+        /// <summary>
+        /// 人员名称
+        /// </summary>		
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 月度，注：保存的是 考核管理记录Id
+        /// </summary>		
+        public int Monthly { get; set; }
+
+        /// <summary>
+        /// 上半年，注：保存的是 考核管理记录Id
+        /// </summary>		
+        public int HalfYear { get; set; }
+
+        /// <summary>
+        /// 季度，注：保存的是 考核管理记录Id
+        /// </summary>		
+        public int Quarter { get; set; }
+
+        /// <summary>
+        /// 年度，注：保存的是 考核管理记录Id
+        /// </summary>		
+        public int Annual { get; set; }
+
+    }
+}
--- src/xschool/GCenter/XSchool.GCenter.Model/KpiTemplateRecord.cs
+++ src/xschool/GCenter/XSchool.GCenter.Model/KpiTemplateRecord.cs
@@ -3,17 +3,14 @@ using XSchool.Core;
 
 namespace XSchool.GCenter.Model
 {
-    /// <summary>
-    /// 考核管理记录
-    /// </summary>
-    public class KpiManageRecord : IModel<int>
+    public class KpiTemplateRecord : IModel<int>
     {
         public int Id { get; set; }
 
         /// <summary>
         /// 考核类型，1-部门，2-人员
         /// </summary>		
-        public KpiType KpiType { get; set; }
+        public int KpiType { get; set; }
 
         /// <summary>
         /// 所属考核方案Id
@@ -24,11 +21,6 @@ namespace XSchool.GCenter.Model
         /// 所属考核方案
         /// </summary>		
         public string KpiName { get; set; }
-
-        /// <summary>
-        /// 考核对象类型，1-部门负责人，2-人员
-        /// </summary>		
-        public KpiObjectType ObjectType { get; set; }
 
         /// <summary>
         /// 公司Id
@@ -61,45 +53,14 @@ namespace XSchool.GCenter.Model
         public string UserName { get; set; }
 
         /// <summary>
-        /// 考核时间
-        /// </summary>		
-        public string KpiDate { get; set; }
-
-        /// <summary>
-        /// 考核分数
-        /// </summary>		
-        public decimal? Score { get; set; }
-
-        /// <summary>
-        /// 当前步骤，[0-自评，1-初审，2-终审，-1-完成]
-        /// </summary>		
-        public KpiSteps Steps { get; set; }
-
-        /// <summary>
-        /// 当前步骤操作人Id
-        /// </summary>		
-        public int? StepsAuditId { get; set; }
-
-        /// <summary>
-        /// 当前步骤操作人姓名
-        /// </summary>		
-        public string StepsAuditName { get; set; }
-
-        /// <summary>
         /// 添加时间
         /// </summary>		
         public DateTime? AddDate { get; set; }
 
         /// <summary>
-        /// 考核完成日期
-        /// </summary>		
-        public DateTime? CompleteDate { get; set; }
-
-        /// <summary>
         /// 状态，0-自评，1-审批中，2-完成，-1-无效
-        /// </summary>
-        public KpiStatus Status { get; set; } = KpiStatus.Zero;
+        /// </summary>		
+        public int Status { get; set; }
 
     }
 }
-

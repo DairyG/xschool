3a49a82313c83d019b4cfba6401f5d2545104d31
悟鱼 <592164556@qq.com>
2019-07-18 21:11:15 +0800

    Auto stash before merge of "master" and "origin/master"

---
+++ src/xschool/GCenter/XSchool.GCenter.Model/ViewModel/KpiTemplateAuditsDto.cs
@@ -0,0 +1,44 @@
+﻿namespace XSchool.GCenter.Model.ViewModel
+{
+    /// <summary>
+    /// 考核模板审核人
+    /// </summary>
+    public class KpiTemplateAuditsDto
+    {
+        /// <summary>
+        /// 当前步骤
+        /// </summary>		
+        public KpiSteps Steps { get; set; }
+
+        /// <summary>
+        /// 公司Id
+        /// </summary>		
+        public int CompanyId { get; set; }
+
+        /// <summary>
+        /// 公司名称
+        /// </summary>		
+        public string CompanyName { get; set; }
+
+        /// <summary>
+        /// 部门Id
+        /// </summary>		
+        public int DptId { get; set; }
+
+        /// <summary>
+        /// 部门名称
+        /// </summary>		
+        public string DptName { get; set; }
+
+        /// <summary>
+        /// 职位Id
+        /// </summary>		
+        public int JobId { get; set; }
+
+        /// <summary>
+        /// 职位名称
+        /// </summary>		
+        public string JobName { get; set; }
+
+    }
+}
---
+++ src/xschool/GCenter/XSchool.GCenter.Model/ViewModel/KpiTemplateContentsDto.cs
@@ -0,0 +1,31 @@
+﻿namespace XSchool.GCenter.Model.ViewModel
+{
+    public class KpiTemplateContentsDto
+    {
+        /// <summary>
+        /// 考核项目Id
+        /// </summary>		
+        public int EvaluationId { get; set; }
+
+        /// <summary>
+        /// 考核项目名称
+        /// </summary>		
+        public string EvaluationName { get; set; }
+
+        /// <summary>
+        /// 所属分类
+        /// </summary>		
+        public string EvaluationType { get; set; }
+
+        /// <summary>
+        /// 权重
+        /// </summary>		
+        public int Weight { get; set; }
+
+        /// <summary>
+        /// 说明
+        /// </summary>		
+        public string Explain { get; set; }
+
+    }
+}
